487d60c662fca9ca75e640bcf82f2df0477994d1
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-03 01:41:23 +0530

    lesson button api to display dynamically show and click event complete and word show

--- javaScript/index.js
+++ javaScript/index.js
@@ -12,6 +12,38 @@ const vocabulariesDataLevel = async () => {
 
 }
 
+// btnclick function 
+const btnWordData = (id) => {
+    // console.log(id)
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((json) => displayWordData(json.data));
+
+}
+
+// btnClick and show all word function
+const displayWordData = (words) => {
+    // console.log(words)
+    const wordContainer = document.getElementById("word-container");
+    // wordContainer.innerHTML = "";
+
+    for (let word of words) {
+        console.log(word)
+
+        const div = document.createElement("div");
+        div.innerHTML = ``;
+
+
+        wordContainer.append(div)
+    }
+
+
+}
+
+
+
 const displayDataLevel = (posts) => {
 
     // 1.access the section display
@@ -20,27 +52,22 @@ const displayDataLevel = (posts) => {
 
     // 2.function create
     posts.forEach(post => {
-        console.log(post)
+        // console.log(post)
 
         // 3.div create and add innerHTML
         const div = document.createElement("div");
         div.innerHTML = `
-              <button class="btn btn-outline btn-primary border-2">
+              <button onCLick="btnWordData(${post.level_no})" class="btn btn-outline btn-primary border-2">
               <i class="fa-solid fa-book-open"></i>Lesson -${post.level_no}</button>
         
         `;
-        
+
         // 4.div append in section
         displayContainer.append(div);
 
 
     });
 
-
-
-
 }
-
-
 
 vocabulariesDataLevel()
